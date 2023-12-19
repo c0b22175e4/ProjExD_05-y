506020700dcff6e5fdc18db19e371c34ba3b6b23
nakanishisan <c0a22109e9@edu.teu.ac.jp>
2023-12-19 16:23:54 +0900

    Blocker

--- karidayo-n.py
+++ karidayo-n.py
@@ -1,28 +1,108 @@
+from pygame import *
 import sys
-import pygame as pg
-import random
+from os.path import abspath, dirname
+from random import choice
+
+BASE_PATH = abspath(dirname(__file__))
+FONT_PATH = BASE_PATH + '/fonts/'
+IMAGE_PATH = BASE_PATH + '/images/'
+SOUND_PATH = BASE_PATH + '/sounds/'
+
+# Colors (R, G, B)
+WHITE = (255, 255, 255)
+GREEN = (78, 255, 87)
+YELLOW = (241, 255, 0)
+BLUE = (80, 255, 239)
+PURPLE = (203, 0, 255)
+RED = (237, 28, 36)
+
+SCREEN = display.set_mode((800, 600))
+FONT = FONT_PATH + 'space_invaders.ttf'
+IMG_NAMES = ['kokaton', 'mystery',
+             'enemy1_1', 'enemy1_2',
+             'enemy2_1', 'enemy2_2',
+             'enemy3_1', 'enemy3_2',
+             'explosionblue', 'explosiongreen', 'explosionpurple',
+             'laser', 'enemylaser']
+IMAGES = {name: image.load(IMAGE_PATH + '{}.png'.format(name)).convert_alpha()
+          for name in IMG_NAMES}
+
+BLOCKERS_POSITION = 450
+ENEMY_DEFAULT_POSITION = 65  # Initial value for a new game
+ENEMY_MOVE_DOWN = 35
+
+class Blocker(sprite.Sprite):
+    def __init__(self, size, color, row, column):
+        sprite.Sprite.__init__(self)
+        self.height = size
+        self.width = size
+        self.color = color
+        self.image = Surface((self.width, self.height))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.row = row
+        self.column = column
+
+    def update(self, keys, *args):
+        game.screen.blit(self.image, self.rect)
 
 
-WIDTH, HEIGHT = 800, 600
+class SpaceInvaders(object):
+    def __init__(self):
+        # It seems, in Linux buffersize=512 is not enough, use 4096 to prevent:
+        #   ALSA lib pcm.c:7963:(snd_pcm_recover) underrun occurred
+        mixer.pre_init(44100, -16, 1, 4096)
+        init()
+        self.clock = time.Clock()
+        self.caption = display.set_caption('Kokaton Invaders')
+        self.screen = SCREEN
+        self.background = image.load(IMAGE_PATH + 'background.jpg').convert()
+        self.startGame = False
+        self.mainScreen = True
+        self.gameOver = False
+        # Counter for enemy starting position (increased each new round)
 
-def main():
-    # ウィンドウの初期化
-    pg.display.set_caption("Kokaton Invader")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("ex05/images/background.png")
+    def reset(self, score):
+        self.keys = key.get_pressed()
 
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT: return
-
-            # 画面の描画
-            screen.blit(bg_img, [0, 0])
-
-            pg.display.update()
+    def make_blockers(self, number):
+        blockerGroup = sprite.Group()
+        for row in range(4):
+            for column in range(9):
+                blocker = Blocker(10, GREEN, row, column)
+                blocker.rect.x = 50 + (200 * number) + (column * blocker.width)
+                blocker.rect.y = BLOCKERS_POSITION + (row * blocker.height)
+                blockerGroup.add(blocker)
+        return blockerGroup
 
 
-if __name__ == "__main__":
-    pg.init()
-    main()
-    pg.quit()
-    sys.exit()
+    @staticmethod
+    def should_exit(evt):
+        # type: (pygame.event.EventType) -> bool
+        return evt.type == QUIT or (evt.type == KEYUP and evt.key == K_ESCAPE)
+
+    def main(self):
+        while True:
+            if self.mainScreen:
+                self.screen.blit(self.background, (0, 0))
+                for e in event.get():
+                    if self.should_exit(e):
+                        sys.exit()
+                    if e.type == KEYUP:
+                        self.allBlockers = sprite.Group(self.make_blockers(0),
+                                                        self.make_blockers(1),
+                                                        self.make_blockers(2),
+                                                        self.make_blockers(3))
+                        self.reset(0)
+                        self.startGame = True
+                        self.mainScreen = False
+
+
+
+            display.update()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    game = SpaceInvaders()
+    game.main()
